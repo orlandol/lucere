bd0ec44c8c8c1b12d3c56beebb8dd8715552a6f6
orlandol <orlandol@veribox.net>
2023-09-08 08:10:09 -0400

    Code restructure.

---
+++ source/c/lucere.c
@@ -0,0 +1,2 @@
+
+#include "lucere.h"
---
+++ source/c/lucere.h
@@ -0,0 +1,46 @@
+#ifndef LUCERE_H
+#define LUCERE_H
+
+/*
+ *  Base declarations
+ */
+
+// App state structure
+typedef struct LucereAppImpl LucereApp;
+
+// Display state structure
+typedef struct LucereDisplayImpl LucereDisplay;
+
+/*
+ *  Abstract declarations
+ */
+typedef struct GraphicsInterface {
+} GraphicsInterface;
+
+///TODO: Define OS specific macros to abstract event routers
+typedef void (*LucereEventRouter)();
+
+enum AppCreateResult {
+  appCreateSuccess = 0,
+  appCreateErrorInTitle = 1,
+  appCreateErrorInWidth = 2,
+  appCreateErrorInHeight = 3,
+  appCreateErrorInFlags = 4,
+  appCreateErrorInEventRouter = 5,
+};
+
+/*
+ *  Win32 specific app declarations
+ */
+LucereApp* win32AppCreate( const char* title, unsigned width, unsigned height,
+  unsigned flags, LucereEventRouter eventRouter, unsigned* result );
+int win32AppRelease( LucereApp** appPointer );
+
+/*
+ *  Win32 GDI specific display declarations
+ */
+LucereDisplay* win32GdiDisplayCreate( LucereApp* app, unsigned monitorID,
+  GraphicsInterface* graphicsSystem );
+int win32GdiDisplayRelease( LucereDisplay** displayPtr );
+
+#endif
